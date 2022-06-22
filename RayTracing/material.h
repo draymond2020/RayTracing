6610d429e290655bf97f8806f120b488ad71ddc1
draymond2018 <draymond2018@gmail.com>
2022-06-23 07:37:45 +0800

    定义一个材质类，主要用来决定材质和光线是如何作用的，例如发生反射，折射、散射 和 能量的衰减

--- RayTracing/material.h
+++ RayTracing/material.h
@@ -8,8 +8,16 @@
 #ifndef material_h
 #define material_h
 
+#include "rtweekend.h"
+
+struct hit_record;
+
+// 材质类主要用来决定材质和光线是如何作用的，例如发生反射，折射、散射 和 能量的衰减
 class material {
-    
+public:
+    // attenuation : 衰减, 能量的衰减
+    // scattered : 散射, 散射出去的光线
+    virtual void scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 };
 
 #endif /* material_h */
