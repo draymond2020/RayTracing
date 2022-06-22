ca67bb66df767372227c1ae660f58505199d33b4
draymond2018 <draymond2018@gmail.com>
2022-06-22 21:44:08 +0800

    定义一个材质，材质主要决定光线碰撞后反射，以及反射后的能量损失

---
+++ RayTracing/material.h
@@ -0,0 +1,15 @@
+//
+//  material.h
+//  RayTracing
+//
+//  Created by 刘清峰 on 2022/6/22.
+//
+
+#ifndef material_h
+#define material_h
+
+class material {
+    
+};
+
+#endif /* material_h */
--- RayTracing/vec3.h
+++ RayTracing/vec3.h
@@ -125,7 +125,7 @@ inline vec3 random_in_unit_sphere() {
 
 inline vec3 random_unit_vector() {
     // return unit_vector(random_in_unit_sphere());
-    auto a = random_double(0, 2*pi);
+    auto a = random_double(0, 2 * pi);
     auto z = random_double(-1, 1);
     auto r = sqrt(1 - z*z);
     return vec3(r*cos(a), r*sin(a), z);
